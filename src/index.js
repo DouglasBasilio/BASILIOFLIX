ad2dfd0996f7917e3d122902ac280b4ae9988c15
DouglasBasilio <basilio.douglas@yahoo.com.br>
2020-07-28 23:29:57 -0300

    Add Page 404

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import { BrowserRouter,  Switch,  Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
-
-const Pagina404 = () => (<div>Página 404</div>)
+import Page404 from './pages/Page404';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -15,7 +14,7 @@ ReactDOM.render(
       <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo}  />
       <Route path="/cadastro/categoria" component={CadastroCategoria}  />
-      <Route component={Pagina404}/>
+      <Route component={Page404}/>
       */não recarrego a página e sim mostro o componente*/
     </Switch>
   </BrowserRouter>,
